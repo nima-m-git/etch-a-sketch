9ffbaf11e96b3ebd8ab6cc7fb82a407061e13849
nima.mirzaei@hotmail.com <nima.mirzaei@hotmail.com>
2020-04-17 14:57:14 -0400

    reset button with custom dimensions

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 const container = document.querySelector('.container');
 console.log(container);
 
-for (i=1 ; i <=16*16; i++){
-    newDiv = document.createElement('div');
-    newDiv.setAttribute('class','grid');
-    container.appendChild(newDiv);
+makeGrid();
+
+function makeGrid(dimension=16) {
+    for (i=1 ; i <=dimension**2; i++){
+        newDiv = document.createElement('div');
+        newDiv.setAttribute('class','grid');
+        let pixelRatio = 100/dimension + '%';
+        newDiv.style.width = pixelRatio;
+        newDiv.style.height = pixelRatio;
+        container.appendChild(newDiv);
+    }
 }
 
 container.addEventListener('mouseover', function(event) {
     event.target.style.backgroundColor = 'purple';
 })
 
+function reset() {
+    child = container.firstElementChild
+    while (child) {
+        container.removeChild(child);
+        child = container.firstElementChild;
+    }
+
+    let newSize = prompt('How many pixels per side?');
+    makeGrid(newSize);
+}
